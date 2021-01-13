ff136a87bb2ddbf46039b984a0505a9e44107a9f
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2021-01-13 19:19:44 +0100

    Prepare for deployment

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')(a^yrov+yt^++ca^a_gbx-@gfvoz1k2ob1s$h82(0n6j*8o)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
