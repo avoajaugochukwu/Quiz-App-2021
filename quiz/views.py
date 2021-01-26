d6b81f694852de0ef250eafad99ef99d8382b78d
Ugochukwu Avoaja <karlthegoo@hotmail.com>
2021-01-26 14:44:40 +0100

    Changed the format of multi-line comments

--- quiz/views.py
+++ quiz/views.py
@@ -45,10 +45,10 @@ def take_quiz(request, test_uuid):
     choices = Choice.objects.all()
     title = 'Quiz App - Take a shot'
 
-    '''
-        unanswered_question will be false for new test
-        If this view is called from submit_test due to unanswered questions
-        Then unanswered_questions will be true, trigerring an alert in the template'''
+    
+    # unanswered_question will be false for new test
+    # If this view is called from submit_test due to unanswered questions
+    # Then unanswered_questions will be true, trigerring an alert in the template
     unanswered_questions = request.session['unanswered_questions']
     print(choices)
     context = {
@@ -67,21 +67,21 @@ def submit_test(request):
 
         test_uuid = request.POST['test_uuid']
 
-        ''' questions_count is used to track number of questions in the quiz
-            We would use it to compare result
-            To know when users didn't finish the test'''
+        # questions_count is used to track number of questions in the quiz
+        # We would use it to compare result
+        # To know when users didn't finish the test
         questions_count = request.POST['questions_count']
 
-        ''' Obtain post keys and values and convert to list
-            post_keys contains the question id
-            post_values holds the option id '''
+        # Obtain post keys and values and convert to list
+        # post_keys contains the question id
+        # post_values holds the option id
         post_keys = list(request.POST.keys())
         post_values = list(request.POST.values())
 
-        ''' Eliminate the first three items in post_keys & post_values
-            Because they are for the csrf, uuid keys and questions_count and values
-            Then convert remaining items in the lists to integers
-            Using list comprehension '''
+        # Eliminate the first three items in post_keys & post_values
+        # Because they are for the csrf, uuid keys and questions_count and values
+        # Then convert remaining items in the lists to integers
+        # Using list comprehension
         list_question_id = [int(i) for i in post_keys[3:]]
         list_option_id = [int(i) for i in post_values[3:]]
         print(list_question_id, list_option_id)
@@ -107,10 +107,10 @@ def submit_test(request):
         quiz.total = total
         quiz.save()
 
-        ''' Loop through list of questions and list of choices from POST request
-            and also choices object queryset
-            So that we can tie all into the Response object,
-            which will help to show case results with highlight for wrong and right answers'''
+        # Loop through list of questions and list of choices from POST request
+        # and also choices object queryset
+        # So that we can tie all into the Response object,
+        # which will help to show case results with highlight for wrong and right answers
         for i, j, k in zip(list_question_id, list_option_id, choices):
             question = Question.objects.get(id=i)
             choice = Choice.objects.get(id=j)
@@ -126,11 +126,11 @@ def submit_test(request):
 # We would eventually pass the quiz_id to this view to use it to filter the Response object
 # in future only questions and options that have related test id will be passed to the frontend
 def result_detail(request, test_uuid):
-    """ Prepare objects for result view
+    # Prepare objects for result view
 
-        In the next few lines of code, we try to minimize the amount of objects that is sent back to the view
-        We exploit the relationships between the Question, Options and Response model to achieve this
-        By tying back every object to the quiz_id of the current user """
+    # In the next few lines of code, we try to minimize the amount of objects that is sent back to the view
+    # We exploit the relationships between the Question, Options and Response model to achieve this
+    # By tying back every object to the quiz_id of the current user
     quiz_answers = QuizAnswer.objects.filter(quiz_id=test_uuid)
 
     # Obtain only questions related to the quiz_id
@@ -157,18 +157,18 @@ def result_detail(request, test_uuid):
 
 
 def result_list(request):
-    """ This filter uses the Django F expression to compare two fields in the same model
-        Here we are comparing the test start and end time, which were initialized with the
-        same value.
+    # This filter uses the Django F expression to compare two fields in the same model
+    # Here we are comparing the test start and end time, which were initialized with the
+    # same value.
 
-        If the test was completed then the end time will be updated at submission.
+    # If the test was completed then the end time will be updated at submission.
 
-        If they are the same, it means the user did not finish the test,
-        because end time is updated when test is completed and submitted.
+    # If they are the same, it means the user did not finish the test,
+    # because end time is updated when test is completed and submitted.
 
-        start__lt=F('end') means: start field is less than end field
+    # start__lt=F('end') means: start field is less than end field
 
-    """
+    
     quizzes = Quiz.objects.filter(start__lt=F('end'))
 
     title = 'Quiz App - View all results'
